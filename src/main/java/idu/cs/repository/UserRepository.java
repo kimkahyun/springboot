3fc66a32c3ed154e5f7c98385ddc6039e2ce6e9c
kimkahyun <krwideweb96@naver.com>
2019-05-02 14:22:56 +0900

    change homecontroller

---
+++ src/main/java/idu/cs/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package idu.cs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import idu.cs.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
