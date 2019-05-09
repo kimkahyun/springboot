4c3552dad298e5d8dff0819750c4496a4f708400
kimkahyun <krwideweb96@naver.com>
2019-05-09 17:35:20 +0900

    5/9

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $("#regform").submit(function (event) {
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+        fire_ajax_submit();
+    });
+
+});
+
+function fire_ajax_submit() {
+
+    //var data = $("#regform").serializeArray();
+    
+	var data = {
+			"name" : $('#name').val(),
+			"company" : $('#company').val()
+			};
+    console.log(data);
+    
+    var json = JSON.stringify(data);
+    
+    console.log(json);
+    //var formArray = {};
+    
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/create",
+        data: json,
+        dataType: 'text', // json -> text
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+        	console.log("SUCCESS :",data);
+        	window.location.href = "/welcome";
+        },
+        error: function (e) {
+
+            console.log("ERROR : ", e);
+
+        }
+    });
+
+}
